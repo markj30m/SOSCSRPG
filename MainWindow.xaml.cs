7925ce36a50f071099e8c9fe243820da0e607251
markj30m <59736485+markj30m@users.noreply.github.com>
2021-07-13 13:43:01 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Windows;
+using Engine.ViewModels;
+using Engine.EventArgs;
+using System.Windows.Documents;
+
+namespace WPFUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private  readonly GameSession _gameSession = new GameSession();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _gameSession.OnMessageRaised += OnGameMessageRaised ;
+
+            DataContext = _gameSession;
+        }
+
+        private void OnClick_MoveNorth(Object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveNorth();
+        }
+
+        private void OnClick_MoveWest(Object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveWest();
+
+        }
+
+        private void OnClick_MoveEast(Object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveEast();
+
+        }
+
+        private void OnClick_MoveSouth(Object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveSouth();
+
+        }
+
+        private void OnClick_AttackMonster(object sender, RoutedEventArgs e)
+        {
+            _gameSession.AttackCurrentMonster();
+        }
+
+        private void OnGameMessageRaised(object sender, GameMessageEventArgs e)
+        {
+            GameMessages.Document.Blocks.Add(new Paragraph(new Run(e.Message)));
+            GameMessages.ScrollToEnd();
+        }
+
+        private void OnClick_DisplayTradeScreen(object sender, RoutedEventArgs e)
+        {
+            TradeScreen tradeScreen = new TradeScreen();
+            tradeScreen.Owner = this;
+            tradeScreen.DataContext = _gameSession;
+            tradeScreen.ShowDialog();
+        }
+    }
+}
